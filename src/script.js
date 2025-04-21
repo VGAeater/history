a06a27cc630e2c897676807371733c7662aec408
VGAeater <stratjel@gmail.com>
2025-04-20 23:19:05 -0400

    changed some stuff

---
+++ src/script.js
@@ -0,0 +1,16 @@
+window.onresize = window.onload = () => {
+	if ( window.innerHeight / window.innerWidth < 1 ) {
+		document.getElementById("burger").style.display = "none";
+		document.getElementById("normalmenu").style.display = "block";
+	} else {
+		document.getElementById("burger").style.display = "block";
+		document.getElementById("normalmenu").style.display = "none";
+	}
+};
+document.body.onclick = () => {
+	document.getElementById("burgermenu").style.display = "none";
+};
+document.getElementById("hamburger").onclick = e => {
+	e.stopPropagation();
+	document.getElementById("burgermenu").style.display = "block";
+};
